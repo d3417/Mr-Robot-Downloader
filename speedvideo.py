14cc248a1fb457d0352e152c86c048b92f99df82
d3417 <denizdifiesso2@gmail.com>
2019-11-24 08:53:34 +0100

    Add files via upload

---
+++ speedvideo.py
@@ -0,0 +1,41 @@
+#StreamSite Import
+import sys
+sys.path.append('')
+
+from streamsite import streamSite
+import requests
+
+class speedVideo(streamSite):
+    def __init__(self):
+        streamSite.__init__(self, "speedvideo")
+
+    def getStreamUrl(self, url):
+        r = requests.get(url)
+        if(r.status_code != 200):
+            print("Error getting url")
+            return
+
+        if '<input type="hidden" name="op" value="' in r.text:
+            op = self.returnFirstReGroup('<input type="hidden" name="op" value="(.*)">', r.text)
+            usr_login = self.returnFirstReGroup('<input type="hidden" name="usr_login" value="(.*)">', r.text)
+            id = self.returnFirstReGroup('<input type="hidden" name="id" value="(.*)">', r.text)
+            fname = self.returnFirstReGroup('<input type="hidden" name="fname" value="(.*)">', r.text)
+            referer = self.returnFirstReGroup('<input type="hidden" name="referer" value="(.*)">', r.text)
+            hash = self.returnFirstReGroup('<input type="hidden" name="hash" value="(.*)">', r.text)
+            
+            site = self.returnFirstReGroup('<Form method="POST" action=\'(.*)\'>', r.text)
+            
+            r = requests.post(site, data = {
+                    'op': op, 
+                    'usr_login': usr_login,
+                    'id': id,
+                    'fname': fname,
+                    'referer': referer,
+                    'hash': hash
+                }
+            )
+
+        streamUrl = self.returnFirstReGroup('var linkfile ="(.*)";', r.text)
+
+        return streamUrl
+
